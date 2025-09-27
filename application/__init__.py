7350135c3f1cddc0f7a477320bcfcb49873e511c
Madlion0187 <norbi0187@gmail.com>
2025-09-27 19:41:56 +0200

    RKR-10: Bejelentkezés kijelentkezés funkciók hozzáadása

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
+db = SQLAlchemy()
+app.config.from_object('config')
+db.init_app(app)
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_message = "Az oldal használatához be kell jelentkezned"
+login_manager.login_message_category = 'danger'
+login_manager.login_view = 'login'
 
 from application import routes
+
+with app.app_context():
+    db.create_all()
