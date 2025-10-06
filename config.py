03d5b38329901576a641ba1bae1523d5973ea838
Madlion0187 <norbi0187@gmail.com>
2025-10-06 17:56:00 +0200

    Adatbázis és táblanév modosítása

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = 'dijsilmeslmelwovwnajpowJ'
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:810826@localhost:5432/database'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:810826@localhost:5432/flask_app_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
