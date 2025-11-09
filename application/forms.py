a249fedfdf66e56522bdf7e1f9b30e408b894680
Madlion0187 <norbi0187@gmail.com>
2025-11-09 15:18:51 +0100

    RKR-11: felhasználói fiók módosítása

--- application/forms.py
+++ application/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from application.models import User
 
 
+# Új felhasználó
 class NewUserForm(FlaskForm):
     username = StringField('Név', validators=[DataRequired(), Length(min=2,max=20)])
     email = StringField('E-mail cím', validators=[DataRequired(), Email(message="A megadott email cím formailag nem megfelelő")])
@@ -11,20 +12,49 @@ class NewUserForm(FlaskForm):
     confirm_password = PasswordField('Jelszó még egyszer', validators=[DataRequired(), EqualTo('password', message="Nem egyezik a fent megadott jelszóval")])
     submit = SubmitField('Mentés')
 
+    # ez a metódus ellenőrzi, hogy van-e már ilyen felhasználónév
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError("Ez a felhasználónév már foglalt, adjon meg másikat")
-        # ez a funkció ellenőrzi, hogy van-e már ilyen felhasználónév
-
+        
+    # ez a metódus ellenőrzi, hogy foglalt-e már a megadott mail cím
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError("Ez az email cím már foglalt, adjon meg másikat")
         
 
+# Bejelentkezés
 class LoginForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired()])
     password = PasswordField('Jelszó', validators=[DataRequired()])
     remember = BooleanField('Bejelentkezve maradok')
     submit = SubmitField('Bejelentkezés')
+
+
+# Felhasználói adatok módosítása (név, email)
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Név', validators=[DataRequired(), Length(min=2,max=20)])
+    submit = SubmitField('Változtatások mentése')
+    cancel = SubmitField('Mégse')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user and user.id != self.original_user.id:
+            raise ValidationError("Ez a felhasználónév már foglalt, adjon meg másikat")
+
+# Felhasználói adatok módosítása (jelszó)
+class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField('Jelenlegi jelszó', validators=[DataRequired()])
+    new_password = PasswordField('Új jelszó', validators=[DataRequired()])
+    confirm_password = PasswordField('Új jelszó megerősítése', validators=[DataRequired(), EqualTo('new_password')])
+    submit = SubmitField('Mentés')
+    cancel = SubmitField('Mégse')
+
+
+# Felhasználói fiók törlése
+class DeleteAccountForm(FlaskForm):
+    password = PasswordField('Jelszó megerősítése', validators=[DataRequired()])
+    confirm = SubmitField('Fiók törlése')
+    cancel = SubmitField('Mégse')
--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request, flash, abort
 from flask_login import login_user, current_user, login_required, logout_user
 from application import app, db, bcrypt
 from application.models import User
-from application.forms import NewUserForm, LoginForm
+from application.forms import NewUserForm, LoginForm, UpdateAccountForm, DeleteAccountForm, ChangePasswordForm
 
 
 @app.route('/')
@@ -11,6 +11,7 @@ def home():
     return render_template('index.html', title='Főoldal')
 
 
+# Regisztráció
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = NewUserForm() # forms.py-ból meghívom az osztálykonstruktort
@@ -25,6 +26,7 @@ def register():
     return render_template('register.html', title="Felhasználói fiók létrehozása", form=form)
 
 
+# Bejelentkezés
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated: # ha a felhasználó be van jelentkezve és a login url-re navigál, a rendszer átirányítja a home-ra
@@ -40,13 +42,79 @@ def login():
     return render_template('login.html', title='Bejelentkezés', form=form)
 
 
+# Kijelentkezés
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
 
-@app.route('/account')
+# Felhasználói fiók + adatok módosítása
+@app.route('/account', methods=["GET", "POST"])
 @login_required
 def account():
-    return render_template('account.html', title='Felhasználói fiók')
+    form = UpdateAccountForm()
+    form.original_user = current_user
+
+    if form.validate_on_submit():
+        if form.cancel.data:
+            flash("A módosítás megszakítva.", "secondary")
+            return redirect(url_for('account'))
+
+        current_user.username = form.username.data
+        db.session.commit()
+        flash("A fiók adatai sikeresen frissültek!", 'success')
+        return redirect(url_for('account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+
+    return render_template('account.html', form=form, title='Felhasználói fiók')
+
+
+# Jelszó módosítás
+@app.route("/change_password", methods=["GET", "POST"])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if form.cancel.data:
+            flash("A jelszó módosítása megszakítva.", "secondary")
+            return redirect(url_for('account'))
+
+        if bcrypt.check_password_hash(current_user.password, form.current_password.data):
+            hashed_pw = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+            current_user.password = hashed_pw
+            db.session.commit()
+            flash('A jelszó sikeresen módosult.', 'success')
+            return redirect(url_for('account'))
+        else:
+            flash('A megadott jelenlegi jelszó hibás.', 'danger')
+    return render_template('change_password.html', form=form)
+
+
+# Felhasználói fiók törlése
+@app.route("/delete_account", methods=["GET", "POST"])
+@login_required
+def delete_account():
+    form = DeleteAccountForm()
+
+    if form.validate_on_submit():
+        # Ha a felhasználó megszakítja
+        if form.cancel.data:
+            flash("A fiók törlése megszakítva.", "secondary")
+            return redirect(url_for('account'))
+        
+        # Jelszavas megerősítés
+        if bcrypt.check_password_hash(current_user.password, form.password.data):
+            user_id = current_user.id
+            logout_user()
+            user = User.query.get(user_id)
+            db.session.delete(user)
+            db.session.commit()
+            flash("A fiók sikeresen törölve.", "info")
+            return redirect(url_for('home'))
+        else:
+            flash("Hibás jelszó! A fiók nem lett törölve.", "danger")
+
+    return render_template('delete_account.html', form=form)
